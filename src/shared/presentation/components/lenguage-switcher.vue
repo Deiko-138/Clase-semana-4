52fa15b97d0ba1fd45f86540df9029de31877755
Deiko-138 <leonardolopezsegundob@gmail.com>
2026-04-20 17:34:44 -0500

    feat(i18n): add language switcher component for locale selection

---
+++ src/shared/presentation/components/lenguage-switcher.vue
@@ -0,0 +1,7 @@
+<script setup>
+import {SelectButton as PvSelectButton} from "primevue";
+</script>
+<template>
+<pv-select-button v-model="$i18n.Locale" : options="$i18n.availableLocales" </template>
+<style scoped>
+</style>
